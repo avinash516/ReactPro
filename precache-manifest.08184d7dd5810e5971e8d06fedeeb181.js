3ef05c529c1ccfa83ddcbaf2b57d7775f51bd6ba
Harideep <hari.apssdc@gmail.com>
2020-04-30 20:30:19 +0530

    Updates

---
+++ precache-manifest.08184d7dd5810e5971e8d06fedeeb181.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d333fe6e76ae2aa52001e6c34d7412c",
+    "url": "/ReactPro/index.html"
+  },
+  {
+    "revision": "b2e8c52ebffd84b4178d",
+    "url": "/ReactPro/static/css/main.b00fc7a3.chunk.css"
+  },
+  {
+    "revision": "7fe17255e6554cb7e49e",
+    "url": "/ReactPro/static/js/2.26f4b651.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPro/static/js/2.26f4b651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e8c52ebffd84b4178d",
+    "url": "/ReactPro/static/js/main.2607ec79.chunk.js"
+  },
+  {
+    "revision": "3725de0b47506fbc7ebf",
+    "url": "/ReactPro/static/js/runtime-main.92921bb2.js"
+  }
+]);
